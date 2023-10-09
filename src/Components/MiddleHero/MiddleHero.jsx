4ae90cbe8dd85931bdc4515eee1c118d6b406792
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-10-09 18:11:45 +0600

    middle-hero responsive fixed

--- src/Components/MiddleHero/MiddleHero.jsx
+++ src/Components/MiddleHero/MiddleHero.jsx
@@ -4,13 +4,13 @@ const MiddleHero = () => {
   return (
 
 
-    <div className="my-16">
+    <div className="my-16 p-6">
   
      
-     <div data-aos="fade-up-left" className="lg:flex justify-center items-center gap-3">
-        <img className="w-1/2 " src="https://i.ibb.co/XjTBn8F/style.png" alt="" />
+     <div data-aos="zoom-in" className="  lg:flex justify-center items-center gap-3">
+        <img className=" lg:w-1/2 " src="https://i.ibb.co/XjTBn8F/style.png" alt="" />
      <div className="flex-1 space-y-12 ">
-     <h2 className="text-3xl font-medium">The Main Reasons to Practice Yoga</h2>
+     <h2 className=" text-2xl lg:text-3xl font-medium">The Main Reasons to Practice Yoga</h2>
       <p className="leading-7  " > Regular yoga practice helps improve flexibility, builds strength, and enhances balance. It can alleviate various physical ailments, reduce the risk of injury, and promote overall physical fitness.Yoga is known for its calming effects on the mind. It reduces stress, anxiety, and depression while increasing mental clarity and focus. The mindfulness cultivated during yoga practice can lead to better emotional balance.</p>
      </div>
     </div>
