d8fc0a7a7398d60f9ea365bb76675b7ffb20a5ce
NazmulHasan000 <nazmulhasanshadin000@gmail.com>
2023-10-09 22:12:09 +0600

    error page updated

--- src/Components/Service/Service.jsx
+++ src/Components/Service/Service.jsx
@@ -17,9 +17,9 @@ const Service = ({service}) => {
   <div data-aos="zoom-in"  data-aos-duration="1000"  className="max-w-sm h-[600px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col">
      <img className="h-[234px]" src={image} alt="" />
     <div className="p-6 flex flex-col flex-grow">
-    <a href="#">
+   
         <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white"> {name}</h5>
-    </a>
+ 
     <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">{description.substring(0,150)}</p>
     <h2 className=""> <span className="font-medium">price</span> : {price}</h2>
     
